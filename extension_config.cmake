3be7dcecbc1743782c86b2333323108f4789b348
Max Gabrielsson <max@gabrielsson.com>
2025-01-08 13:01:17 +0100

    use extension ci tools makefile

---
+++ extension_config.cmake
@@ -0,0 +1,11 @@
+# This file is included by DuckDB's build system. It specifies which extension to load
+
+# Extension from this repo
+duckdb_extension_load(postgres_scanner
+    SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
+    DONT_LINK
+    LOAD_TESTS
+)
+
+# Any extra extensions that should be built
+# e.g.: duckdb_extension_load(json)
